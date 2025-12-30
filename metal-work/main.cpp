866566f29b16f8c3224eb8103702d52af8e8e48f
nishant <nishun2005@gmail.com>
2025-12-30 14:11:39 -0800

    Initial Commit

---
+++ metal-work/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metal-work
+//
+//  Created by Nishant Nuthalapati on 12/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
